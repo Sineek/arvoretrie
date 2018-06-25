504351d476a44fecca38d52a7b3e20c6bf0a4e3b
Joyce Mayara <joycemayara.al@gmail.com>
2018-06-25 18:16:14 -0300

    Inserção da função de criar nó e adicionar palavra.

--- ArvoreTrie/trie.h
+++ ArvoreTrie/trie.h
@@ -1,6 +1,15 @@
 #ifndef TRIE_H
 #define	TRIE_H
 
+#define TAMANHO_ALFABETO (5)
+#define TAMANHO_PALAVRA (30)
+
+struct no { 
+	char tipoNo; // 'I': nó interno | 'P': nó palavra
+	struct no* filho[TAMANHO_ALFABETO];	
+};
+
+typedef struct no no; 
 
 #endif	/* TRIE_H */
 
