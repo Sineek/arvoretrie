6e752fcbf7616715501550593345470389b6429b
Gabriel Félix <gabriel.felixramos@gmail.com>
2018-06-25 17:02:28 -0300

    aaa

--- ArvoreTrie/nbproject/Makefile-Debug.mk
+++ ArvoreTrie/nbproject/Makefile-Debug.mk
@@ -35,7 +35,8 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
-	${OBJECTDIR}/main.o
+	${OBJECTDIR}/main.o \
+	${OBJECTDIR}/trie.o
 
 
 # C Compiler Flags
@@ -66,6 +67,11 @@ ${OBJECTDIR}/main.o: main.c
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} "$@.d"
 	$(COMPILE.c) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main.o main.c
+
+${OBJECTDIR}/trie.o: trie.c 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.c) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/trie.o trie.c
 
 # Subprojects
 .build-subprojects:
