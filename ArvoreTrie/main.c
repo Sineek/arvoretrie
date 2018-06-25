978aed75adfc829fd14a76cfe118d46a6c41a995
Gabriel Félix <gabriel.felixramos@gmail.com>
2018-06-25 17:17:15 -0300

    menu feio na main

--- ArvoreTrie/main.c
+++ ArvoreTrie/main.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- * 
- */
 int main() {
-    printf("hello");
+    int escolha = 0;
+    
+    while (escolha != 3) {
+        printf("[1] Gerar trie\n");
+        printf("[2] Testar aplicação\n");
+        printf("[3] Sair\n");
+        scanf("%d", &escolha);
+    }
+    switch (escolha) {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+    }
 
     return 0;
 }
