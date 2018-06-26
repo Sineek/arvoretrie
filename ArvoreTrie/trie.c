961f001008481fc02c6cf6e3522f1e65a83ca341
Joyce Mayara <joycemayara.al@gmail.com>
2018-06-26 10:40:04 -0300

    [ADD] Comentários na função de adicionar_palavra para maior clareza do código.

--- ArvoreTrie/trie.c
+++ ArvoreTrie/trie.c
@@ -14,58 +14,40 @@ no trie_criarNo() {
     }
 }
 
+/* Percorre a TRIE, comparando os elementos da palavra e os prefixos já existentes na árvore. 
+Se o prefixo em questão não estiver no ramo avaliado, cria-se um nó para conter o prefixo não existente e assim sucessivamente, 
+até que a palavra esteja criada. */
 void trie_adicionarPalavra(char* palavra, no* raiz) {
     int nivel = 0;
     int indice = 0;
     no* aux = raiz;
 
-    /* Como o alfabeto não segue o padrão, deve-se calcular a posição (índice)
+    /* Como o alfabeto não segue o padrãodo alfabeto português de 26 letras, deve-se calcular a posição (índice)
     de cada elemento da palavra em relação ao alfabeto */
     for (nivel = 0; nivel < strlen(palavra); nivel++) {
-        if (palavra[nivel].strcmp("a")) {
+        if (palavra[nivel].strcmp("a")) { // Letra 'a' está na posição 0 no alfabeto
             indice = 0;
         }
-        if (palavra[nivel].strcmp("b")) {
+        if (palavra[nivel].strcmp("b")) { // Letra 'b' está na posição 1 no alfabeto
             indice = 1;
         }
-        if (palavra[nivel].strcmp("m")) {
+        if (palavra[nivel].strcmp("m")) { // Letra 'm' está na posição 2 no alfabeto
             indice = 2;
         }
-        if (palavra[nivel].strcmp("i")) {
+        if (palavra[nivel].strcmp("i")) { // Letra 'i' está na posição 3 no alfabeto
             indice = 3;
         }
-        if (palavra[nivel].strcmp("o")) {
+        if (palavra[nivel].strcmp("o")) { // Letra 'o' está na posição 4 no alfabeto
             indice = 4;
         }
-        /* Chegou-se no ponto onde um dos elementos da palavra não possui um nó equivalente. Deve-se criar um novo nó. */
+        /* Chegou-se no ponto onde um dos elementos da palavra não possui um nó/prefixo equivalente presente na árvore.
+        Deve-se criar um novo nó para conter o elemento/prefixo não existente. */
         if (aux->filho[indice] == NULL) {
             aux->filho[indice] = trie_criarNo();
         }
-        // Desce na árvore entre as posições já existentes do alfabeto, e caso contrário, cria um nó para cada letra não existente
+        // Atualização de ponteiros para caminhar na TRIE: Desce na árvore entre os prefixos já existentes no ramo
         aux = aux->filho[indice];
     }
     aux->tipo = 'P'; // Criou-se a palavra em uma folha. 
 }
 
-int trie_calculaPosicao(char* palavra) {
-    int nivel = 0;
-    int posicao; // posição da letra em relação ao alfabeto. 
-
-    for (nivel = 0; nivel < strlen(palavra); nivel++) {
-        if (palavra[nivel].strcmp("a")) {
-            posicao = 0;
-        }
-        if (palavra[nivel].strcmp("b")) {
-            posicao = 1;
-        }
-        if (palavra[nivel].strcmp("m")) {
-            posicao = 2;
-        }
-        if (palavra[nivel].strcmp("i")) {
-            posicao = 3;
-        }
-        if (palavra[nivel].strcmp("o")) {
-            posicao = 4;
-        }
-    }
-}
