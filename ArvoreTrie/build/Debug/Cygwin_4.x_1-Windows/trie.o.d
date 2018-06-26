6ee9eb1cadbde94c4d652d79c8633a3a5dac7fbd
Gabriel Félix <gabriel.felixramos@gmail.com>
2018-06-26 17:07:43 -0300

    aaa

--- ArvoreTrie/build/Debug/Cygwin_4.x_1-Windows/trie.o.d
+++
@@ -1,1 +0,0 @@
-build/Debug/Cygwin_4.x_1-Windows/trie.o: trie.c
--- ArvoreTrie/trie.c
+++ ArvoreTrie/trie.c
@@ -69,19 +69,19 @@ int trie_buscaPalavra(char* palavra, no* raiz) {
     /* Como o alfabeto não segue o padrão do alfabeto português de 26 letras, deve-se calcular a posição (índice)
     de cada elemento da palavra em relação ao alfabeto */
     for (i = 0; i < strlen(palavra); i++) {
-        if (palavra[i].strcmp("A")) { // Letra 'A' está na posição 0 no alfabeto/vetor
+        if (strcmp(palavra[i], "A") == 0) { // Letra 'A' está na posição 0 no alfabeto/vetor
             indice = 0;
         }
-        if (palavra[i].strcmp("B")) { // Letra 'B' está na posição 1 no alfabeto/vetor
+        if (strcmp(palavra[i], "B") == 0) { // Letra 'B' está na posição 1 no alfabeto/vetor
             indice = 1;
         }
-        if (palavra[i].strcmp("M")) { // Letra 'M' está na posição 2 no alfabeto/vetor
+        if (strcmp(palavra[i], "M") == 0) { // Letra 'M' está na posição 2 no alfabeto/vetor
             indice = 2;
         }
-        if (palavra[i].strcmp("I")) { // Letra 'I' está na posição 3 no alfabeto/vetor
+        if (strcmp(palavra[i], "I") == 0) { // Letra 'I' está na posição 3 no alfabeto/vetor
             indice = 3;
         }
-        if (palavra[i].strcmp("O")) { // Letra 'O' está na posição 4 no alfabeto/vetor
+        if (strcmp(palavra[i], "O") == 0) { // Letra 'O' está na posição 4 no alfabeto/vetor
             indice = 4;
         }
 
