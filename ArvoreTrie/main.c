af4c889e83d24ed7c06f200f0d139cf5c10d91fa
Gabriel Félix <gabriel.felixramos@gmail.com>
2018-06-26 18:25:51 -0300

    aaa

--- ArvoreTrie/main.c
+++ ArvoreTrie/main.c
@@ -7,13 +7,16 @@ int main() {
     int escolha = 0;
     no* arvore;
     char nomeArquivoEnt[51];
+    char palavra[TAMANHO_PALAVRA];
+    int cont = 0;
 
     arvore = trie_criarNo();
 
-    while (escolha != 3) {
+    while (escolha != 4) {
         printf("[1] Gerar trie; \n");
-        printf("[2] Testar aplicação; \n");
-        printf("[3] Sair. \n");
+        printf("[2] Pesquisar na trie; \n");
+        printf("[3] Testar aplicação; \n");
+        printf("[4] Sair. \n");
         scanf("%d", &escolha);
         switch (escolha) {
             case 1:
@@ -23,8 +26,27 @@ int main() {
                 trie_gerarArvore(arvore, nomeArquivoEnt);
                 break;
             case 2:
+                printf("Entre com a palavra, tia vanessa");
+                scanf(" %s", palavra);
+                for (int i = 0; i < strlen(palavra); i++) {
+                    if ((palavra[i] = 'A') || (palavra[i] = 'B') || (palavra[i] = 'M') || (palavra[i] = 'I') || (palavra[i] = 'O')) {
+                        cont++;
+                    }
+                }
+
+                if (cont == strlen(palavra)) {
+                    if (trie_buscaPalavra(palavra, arvore) == 1) {
+                        printf("Achamos sua palavra!! aee");
+                    } else {
+                        printf("Palavra não encontrada");
+                    }
+                } else {
+                    printf("Não achamos sua palavra. :/");
+                }
                 break;
             case 3:
+                break;
+            case 4:
                 break;
         }
     }
