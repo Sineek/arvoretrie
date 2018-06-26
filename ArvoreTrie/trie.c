5436bbe36fc53a09abcc62cf8509fc8e6eeda9a1
Joyce Mayara <joycemayara.al@gmail.com>
2018-06-26 14:39:36 -0300

    [ADD] Função para buscar uma palavra; [ADD] Cabeçalhos das funções no trie.h;

--- ArvoreTrie/trie.c
+++ ArvoreTrie/trie.c
@@ -2,16 +2,20 @@
 #include <string.h>
 #include "trie.h"
 
+/* Cria um nó vazio da árvore TRIE */
 no trie_criarNo() {
     int i;
 
-    no* novoNo = (*no) calloc(1, sizeof (no)); // Aloca-se um novo nó da trie
+    no* novoNo = (*no) calloc(1, sizeof (no)); // Aloca-se um novo nó da trie.
 
     if (novoNo != NULL) {
         for (i = 0; i < TAMANHO_ALFABETO; i++) {
-            novoNo->filho[i] = NULL; // Inicializando o nó. 
+            novoNo->filho[i] = NULL; // Inicializando o nó: cada campo tem valor nulo. 
         }
     }
+    novoNo->tipo = '';
+
+    return novoNo; 
 }
 
 /* Percorre a TRIE, comparando os elementos da palavra e os prefixos já existentes na árvore. 
@@ -22,22 +26,22 @@ void trie_adicionarPalavra(char* palavra, no* raiz) {
     int indice = 0;
     no* aux = raiz;
 
-    /* Como o alfabeto não segue o padrãodo alfabeto português de 26 letras, deve-se calcular a posição (índice)
+    /* Como o alfabeto não segue o padrão do alfabeto português de 26 letras, deve-se calcular a posição (índice)
     de cada elemento da palavra em relação ao alfabeto */
     for (nivel = 0; nivel < strlen(palavra); nivel++) {
-        if (palavra[nivel].strcmp("a")) { // Letra 'a' está na posição 0 no alfabeto
+        if (palavra[nivel].strcmp("A")) { // Letra 'a' está na posição 0 no alfabeto
             indice = 0;
         }
-        if (palavra[nivel].strcmp("b")) { // Letra 'b' está na posição 1 no alfabeto
+        if (palavra[nivel].strcmp("B")) { // Letra 'b' está na posição 1 no alfabeto
             indice = 1;
         }
-        if (palavra[nivel].strcmp("m")) { // Letra 'm' está na posição 2 no alfabeto
+        if (palavra[nivel].strcmp("M")) { // Letra 'm' está na posição 2 no alfabeto
             indice = 2;
         }
-        if (palavra[nivel].strcmp("i")) { // Letra 'i' está na posição 3 no alfabeto
+        if (palavra[nivel].strcmp("I")) { // Letra 'i' está na posição 3 no alfabeto
             indice = 3;
         }
-        if (palavra[nivel].strcmp("o")) { // Letra 'o' está na posição 4 no alfabeto
+        if (palavra[nivel].strcmp("O")) { // Letra 'o' está na posição 4 no alfabeto
             indice = 4;
         }
         /* Chegou-se no ponto onde um dos elementos da palavra não possui um nó/prefixo equivalente presente na árvore.
@@ -48,6 +52,50 @@ void trie_adicionarPalavra(char* palavra, no* raiz) {
         // Atualização de ponteiros para caminhar na TRIE: Desce na árvore entre os prefixos já existentes no ramo
         aux = aux->filho[indice];
     }
-    aux->tipo = 'P'; // Criou-se a palavra em uma folha. 
+    aux->tipo = 'P'; // Criou-se a palavra. 
+}
+
+/* Função responsável por verificar se uma palavra se encontra ou não na árvore. Retorna o número 1 em caso afirmativo, e 0
+caso contrário. A função consecutivamente captura cada elemento da palavra, calcula a sua posição/índice equivalente no próximo
+nó e passa a avaliar esse nó, verificando assim se todos os elementos formadores da palavra estão presentes sequencialmente em 
+um dos caminhos da árvore (busca com sucesso). */
+int trie_buscaPalavra(char* palavra, no* raiz) {
+	int i = 0;
+	int indice = 0; // Posição que a palavra ocupa em relação ao alfabeto 
+	no* aux = raiz; // nó auxiliar para caminhar na árvore
+
+	/* Como o alfabeto não segue o padrão do alfabeto português de 26 letras, deve-se calcular a posição (índice)
+	de cada elemento da palavra em relação ao alfabeto */
+	for (i = 0; i < strlen(palavra); i++) {
+	    if (palavra[i].strcmp("A")) { // Letra 'A' está na posição 0 no alfabeto/vetor
+	        indice = 0;
+	    }
+	    if (palavra[i].strcmp("B")) { // Letra 'B' está na posição 1 no alfabeto/vetor
+	        indice = 1;
+	    }
+	    if (palavra[i].strcmp("M")) { // Letra 'M' está na posição 2 no alfabeto/vetor
+	        indice = 2;
+	    }
+	    if (palavra[i].strcmp("I")) { // Letra 'I' está na posição 3 no alfabeto/vetor
+	        indice = 3;
+	    }
+	    if (palavra[i].strcmp("O")) { // Letra 'O' está na posição 4 no alfabeto/vetor
+	        indice = 4;
+	    }
+
+	    /* Desce na árvore comparando os prefixos existentes no caminho com cada elemento da palavra, de modo semelhante à
+	    trie_adicionarPalavra.   */
+	    if(aux->filho[indice] != NULL) {
+	    	aux = aux->filho[indice]; 
+	    } else {
+	    	return 0; 
+	    }	    
+	}
+
+	if(palavra[i] == '\0' && aux->tipo == 'P') {
+		return 1; // A palavra se encontra na árvore. 
+	} else {
+		return 0; // A palavra não se encontra na árvore. 
+	}
 }
 
--- ArvoreTrie/trie.h
+++ ArvoreTrie/trie.h
@@ -1,15 +1,36 @@
+#include <stdlib.h>
+#include <string.h>
+
 #ifndef TRIE_H
 #define	TRIE_H
 
-#define TAMANHO_ALFABETO (5)
+#define TAMANHO_ALFABETO (6) /* Alfabeto formado pelas letras maiúsculas ABMIO mais um elemento para significar o fim do alfabeto. */
 #define TAMANHO_PALAVRA (30)
 
 struct no { 
-	char tipoNo; // 'I': nó interno | 'P': nó palavra
+	char tipoNo; // Pode ser 'I': nó interno ou 'P': nó de informação/palavra
 	struct no* filho[TAMANHO_ALFABETO];	
 };
 
 typedef struct no no; 
+
+no trie_criarNo(); 
+void trie_adicionarPalavra(char* palavra, no* raiz);
+int trie_buscaPalavra(char* palavra, no* raiz); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #endif	/* TRIE_H */
 
